67c003e67ad180f81d265e10022184e8aee9ab70
apurbahasanj <apurbahasanj@gmail.com>
2024-03-17 21:54:27 +0600

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import PageHeader from "@/components/PageHeader";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "DUARUQYAH | Duas",
+  title: "Duaruqyah",
   description:
     "Islam: Faith of peace, compassion, justice, and spiritual enlightenment.",
 };
@@ -14,24 +14,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </head>
       <body className={`${inter.className} fixed left-0 right-0 bg-slate-100`}>
         <div className="lg:hidden bg-white z-[1000]">
           <PageHeader />
         </div>
         <div className="w-full relative lg:px-10 px-5 lg:py-6 py-2 flex lg:flex-row flex-col-reverse gap-6">
           <div className="lg:block hidden ">
-            <Navbar />
+          <Navbar />
           </div>
           <div className="w-full">
             <div className="lg:block hidden">
               <PageHeader />
             </div>
             {children}
+            
           </div>
+          
         </div>
-        <div className="lg:hidden sticky bottom-0 left-0 ">
+        <div className="lg: sticky bottom-0 left-0 ">
           <Navbar />
-        </div>
+          </div>
       </body>
     </html>
   );
