b7dcb38bcaf6334726c3ff4d26f3f23a07398798
apurbahasanj <apurbahasanj@gmail.com>
2024-03-17 17:47:05 +0600

    resposive done

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,21 +20,25 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={`${inter.className} fixed left-0 right-0 bg-slate-100`}>
-        <div>
-          <div className="lg:hidden bg-white z-[1000]">
-            <PageHeader />
-          </div>
+        <div className="lg:hidden bg-white z-[1000]">
+          <PageHeader />
         </div>
-        <div className="w-full relative lg:px-10 px-5 lg:py-6 py-2 flex gap-6">
+        <div className="w-full relative lg:px-10 px-5 lg:py-6 py-2 flex lg:flex-row flex-col-reverse gap-6">
+          <div className="lg:block hidden ">
           <Navbar />
+          </div>
           <div className="w-full">
             <div className="lg:block hidden">
               <PageHeader />
             </div>
-
             {children}
+            
           </div>
+          
         </div>
+        <div className="lg:hidden sticky bottom-0 left-0 ">
+          <Navbar />
+          </div>
       </body>
     </html>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import { FaHandHoldingHeart } from "react-icons/fa6";
 import LogoAndName from "@/components/LogoAndName";
 
-
 const Navbar = () => {
   return (
-    <nav className="lg:flex hidden lg:flex-col items-center justify-between lg:h-[calc(100dvh-48px)] px-5 py-6 bg-white rounded-3xl shadow">
-      <LogoAndName/>
-      <ul className="flex flex-col gap-5">
+    <nav className="lg:flex lg:flex-col items-center justify-between lg:h-[calc(100dvh-48px)] drop-shadow-md px-5 py-6 bg-white lg:rounded-3xl shadow">
+      <div className="lg:block hidden">
+        <LogoAndName />
+      </div>
+      <ul className="flex lg:flex-col justify-between gap-5">
         <li className="nav-item">
           <Image width={25} height={25} src="/home.png" alt="home" />
         </li>
@@ -28,7 +29,7 @@ const Navbar = () => {
           <Image width={25} height={25} src="/dua-info.png" alt="dua-info" />
         </li>
       </ul>
-      <FaHandHoldingHeart className="bg-primary lg:block hidden text-5xl text-white rounded-xl shadow-xl shadow-green-100"/>
+      <FaHandHoldingHeart className="bg-primary lg:block hidden text-5xl text-white rounded-xl shadow-xl shadow-green-100" />
     </nav>
   );
 };
